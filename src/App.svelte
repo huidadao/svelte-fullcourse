7d6c052128a0bbb3ffdcd16478121cd4a40d1706
huidadao <huidadao@gmail.com>
2022-10-13 20:28:43 +0900

    ProgressBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,24 @@
-<h1>Test</h1>
+<script>
+    import Timer from './components/Timer.svelte'
+    import HowTo from './components/HowTo.svelte'
+</script>
+
+
+<h1>Handwashing App</h1>
+
+<Timer />
+
+<HowTo />
+
+<h3>
+    <a href="http://www.who.int/gpsc/clean_hands_protection/en">Picture source</a>
+</h3>
+<h3>
+    <a href="http://freesound.org/people/metrostock99/sounds/345086/">Sound source</a>
+</h3>
+
+<style>
+    h1, h3 {
+        text-align: center;
+    }
+</style>
